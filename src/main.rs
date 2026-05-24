60e9508ae43d444c0949af8139012126086a3cd0
NotPppp116 <NotPppp116@users.noreply.github.com>
2026-05-24 13:13:32 +0100

    dectypr

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,43 @@ mod compression;
 mod encryption;
 mod file;
 mod utils;
-// name --dir <path> 
-
+// name --dir <path>
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args[2] == "--dir" {
-        if !args[3].is_empty() {
-            let file_blob = file::read_dir(&PathBuf::from(&args[3])).unwrap();
-            let metadata_blob = file::populate_metadata(&file_blob);
-            let head = file::create_archive(&metadata_blob);
+    if args[2] == "--enc" && !args[3].is_empty() {
+        if args[3] == "--dir" {
+            if !args[4].is_empty() {
+                let file_blob = file::read_dir(&PathBuf::from(&args[4])).unwrap();
+                let metadata_blob = file::populate_metadata(&file_blob);
+                let head = file::create_archive(&metadata_blob);
 
-            //use this for encryption
-            let mut final_blob = Vec::new();
+                //use this for encryption
+                let mut final_blob = Vec::new();
 
-            file::append_struct(&mut final_blob, &head);
+                file::append_struct(&mut final_blob, &head);
 
-            for meta in &metadata_blob {
-                file::append_struct(&mut final_blob, meta);
+                for meta in &metadata_blob {
+                    file::append_struct(&mut final_blob, meta);
+                }
+
+                for file in &file_blob {
+                    final_blob.extend_from_slice(file.name.as_bytes());
+                }
+
+                for file in &file_blob {
+                    final_blob.extend_from_slice(file.path.to_string_lossy().as_bytes());
+                }
+
+                for file in &file_blob {
+                    final_blob.extend_from_slice(&file.content);
+                }
+
+                encryption::encrypt_and_compress_flow(&mut final_blob);
             }
-
-            for file in &file_blob {
-                final_blob.extend_from_slice(file.name.as_bytes());
-            }
-
-            for file in &file_blob {
-                final_blob.extend_from_slice(file.path.to_string_lossy().as_bytes());
-            }
-
-            for file in &file_blob {
-                final_blob.extend_from_slice(&file.content);
-            }
-
-            encryption::encrypt_and_compress_flow(&mut final_blob);
-            
+        } else if args[2] == "dec" {
+            //continue dectypt
         }
     } else {
         println!("launch with args\n");
